61d2b1417579a963ddb724a6f5d730a99b02968d
rajeswari-subramanian <rajilechrame@gmail.com>
2020-10-11 01:57:47 +0530

    UI-Registerpage-created

--- expense-app/src/Pages/Register.jsx
+++ expense-app/src/Pages/Register.jsx
@@ -5,65 +5,116 @@ import { registerUser } from "../Redux/action";
 import styles from './pages.module.css'
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import styled from 'styled-components'
+
+const Label = styled.div`
+text-align:left;
+display:block;
+`
+const FieldWrap = styled.div`
+border-bottom: 0.5px solid black;
+padding:3px;
+`
+const Button = styled.button`
+width:300px;
+height:50px;
+color:white;
+background:#F65636;
+border-radius:25px
+`
 
 class Register extends React.Component {
     constructor(props) {
-        super(props)        
+        super(props)
     }
     handleSubmit = ({ name, email, password }) => {
-            let payload = {
+        let payload = {
             name: name,
             email: email,
             password: password
         }
         this.props.registerUser(payload)
     }
-    render() {       
+    render() {
         const { registrationStatus, isRegister, isError } = this.props
         //console.log("userdata", registrationStatus, isRegister, isError)      
         return (
-            <React.Fragment>
-                <Formik
-                    initialValues={{ name: '', email: '', password: '' }}
-                    validationSchema={Yup.object({
-                        name: Yup.string()
-                            .min(4, 'Must be 4 characters or more')
-                            .required('Required'),
-                        email: Yup.string().email('Invalid email address').required('Required'),
-                        password: Yup.string()
-                            .min(6, 'Must be 6 characters or more')
-                            .required('Required'),
-                    })}
-                    onSubmit={(values, { setSubmitting }) => {
-                        this.handleSubmit(values)
-                        setTimeout(() => {
-                            //alert(registrationStatus);
-                            setSubmitting(false);
-                        }, 400);
-                    }}
-                >
-                    <Form>
-                        <label htmlFor="name">Name</label>
-                        <Field name="name" type="text" /><br />
-                        <ErrorMessage name="name" /><br />
-                        <label htmlFor="password">Password</label>
-                        <Field name="password" type="password" /><br />
-                        <ErrorMessage name="password" /><br />
-                        <label htmlFor="email">Email Address</label>
-                        <Field name="email" type="email" /><br />
-                        <ErrorMessage name="email" /><br />
-                        <button type="submit">Submit</button>
-                    </Form>
-                </Formik>
-                {isError &&
-                    <div>Account already exists</div>
-                }
-                {isRegister &&
-                    <div>
-                        {registrationStatus}
+            <div class="container" style={{ boxShadow: "5px 10px 18px #888888", background: "#E2DBCC" }}>
+                <div class="row">
+                    <div class="col-4">
+                        <div class="row">
+                            <div class="col-12">
+                                <Formik style={{ background: "#E2DBCC" }}
+                                    initialValues={{ name: '', email: '', password: '' }}
+                                    validationSchema={Yup.object({
+                                        name: Yup.string()
+                                            .min(4, 'Must be 4 characters or more')
+                                            .required('Required'),
+                                        email: Yup.string().email('Invalid email address').required('Required'),
+                                        password: Yup.string()
+                                            .min(6, 'Must be 6 characters or more')
+                                            .required('Required'),
+                                    })}
+                                    onSubmit={(values, { setSubmitting }) => {
+                                        this.handleSubmit(values)
+                                        setTimeout(() => {
+                                            setSubmitting(false);
+                                        }, 400);
+                                    }}
+                                >
+                                    <Form style={{ padding: "30px", background: "#DED9D2" }}>
+                                        <Label htmlFor="name">Name</Label>
+                                        <FieldWrap>
+                                            <Field style={{
+                                                border: "none",
+                                                width: "300px",
+                                                background: "#E2DBCC"
+                                            }} name="name"
+                                                type="text" />
+                                        </FieldWrap>
+                                        <ErrorMessage name="name" /><br />
+                                        <Label htmlFor="password">Password</Label>
+                                        <FieldWrap>
+                                            <Field style={{
+                                                border: "none",
+                                                width: "300px",
+                                                background: "#E2DBCC"
+                                            }} name="password" type="password" /><br />
+                                        </FieldWrap>
+                                        <ErrorMessage name="password" /><br />
+                                        <Label htmlFor="email">Email Address</Label>
+                                        <FieldWrap>
+                                            <Field style={{
+                                                border: "none",
+                                                width: "300px",
+                                                background: "#E2DBCC"
+                                            }} name="email" type="email" /><br />
+                                        </FieldWrap>
+                                        <ErrorMessage name="email" /><br /><br />
+                                        <Button>SIGNUP</Button><br />
+                                        <Link to={'/login'}>Already Member? Sign In</Link>
+                                    </Form>
+                                </Formik>
+                                {isError &&
+                                    <div>Account already exists</div>
+                                }
+                                {isRegister &&
+                                    <div>
+                                        {registrationStatus}
+                                    </div>
+                                }
+                            </div>
+                        </div>
                     </div>
-                }
-            </React.Fragment >
+                    <div class="col-1">
+                        <div class="row">
+                            <div class="col-12">
+                                <img src='icon.jpg' style={{ width: "750px" }} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
