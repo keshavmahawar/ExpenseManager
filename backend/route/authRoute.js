e40dde5a5eafbe130430ae93bbe6c755c338fc28
Keshav Mahawar <kes.mahawar@gmail.com>
2020-10-11 01:00:24 +0530

    deleted useless root file

--- backend/route/authRoute.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-const { registerUser, loginUser } = require("../controller/UserController");
-
-let router = express.Router();
-
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-
-module.exports = router;
